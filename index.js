e04508f054322926ad637a611a8d4116436750e3
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-11-06 16:44:36 +0100

    Set up basic cors for testing

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { createServer } from "node:http";
 import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
 import { Server } from "socket.io";
+import cors from "cors";
 
 const app = express();
 const server = createServer(app);
@@ -10,8 +11,20 @@ const io = new Server(server);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+// cors setup
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
+});
+
+app.get("/test", (req, res) => {
+  res.json("Hey");
 });
 
 io.on("connection", (socket) => {
